29247061156cd73aa4047602e15d1dc345f0a458
Abhishek-Khot <khotabhishek15@gmail.com>
2025-04-14 13:34:31 +0530

    added the dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // this is the default, just here for clarity
+  },
+  base: "/", // ensures proper asset paths in production
 })
